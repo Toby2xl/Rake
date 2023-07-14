0d88ed285e1a12107f35142d4eaa71ad50105af1
Toby2xl <tobyononaku@gmail.com>
2023-07-14 11:45:23 +0100

    Update Customer.cs

--- src/Inventory/Inventory.Core/Entities/Customer.cs
+++ src/Inventory/Inventory.Core/Entities/Customer.cs
@@ -1,7 +1,24 @@
 using System;
 
+using Inventory.Core.Common;
+
 namespace Inventory.Core.Entities;
 
-public class Customer
+public class Customer : Entity<Guid>
 {
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    public int TenantId { get; set; }
+    public int BranchId { get; set; }
+
+    public Guid? StockSalesId { get; set; }
+
+    public Customer()
+    {
+        Id = Guid.NewGuid();
+    }
 }
